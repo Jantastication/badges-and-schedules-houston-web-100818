96b735ef1645e0a56a7d72edbdf71605983d382b
Jantastication O <georginaoregx@gmail.com>
2018-10-03 20:13:32 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(array)
+  new_array = []
+  array.each do |name|
+    new_array<< ("Hello, my name is #{name}")
+  end
+  new_array
+end
+
+def assign_rooms(array)
+  new_array = []
+  counter = 1
+  array.each do |name|
+    new_array<< ("Hello, my name is #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+  end
+  new_array
+end
+
+def printer
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
+end
